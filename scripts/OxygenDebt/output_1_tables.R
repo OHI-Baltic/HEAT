bca3975c10e6da509a23855781951a42b7b0e90a
colin millar <colinpmillar@gmail.com>
2016-12-19 13:10:54 +0100

    work on table of indicators

--- scripts/OxygenDebt/output_1_tables.R
+++ scripts/OxygenDebt/output_1_tables.R
@@ -21,7 +21,14 @@ if (check != "pars") {
 rm(check)
 
 
-sapply(pars, function(parsy) {
-  with(parsy, tapply(oxygendebt, Basin, mean, na.rm = TRUE))
-})
+tab1 <-
+  sapply(pars, function(parsy) {
+    with(parsy, tapply(oxygendebt, Basin, mean, na.rm = TRUE))
+  })
 
+# write out
+write.csv(tab1, file = "analysis/output/OxygenDebt/indicator_table.csv", row.names = FALSE)
+
+# done -------------------
+
+message(sprintf("time elapsed: %.2f seconds", (proc.time() - t0)["elapsed"]))
