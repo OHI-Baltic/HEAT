dedb7567005b3ae8970eb183becb949a383427ba
colin millar <colinpmillar@gmail.com>
2016-12-19 13:14:20 +0100

    indicators by year done

--- scripts/OxygenDebt/output_1_tables.R
+++ scripts/OxygenDebt/output_1_tables.R
@@ -16,18 +16,18 @@ t0 <- proc.time()
 # load gam fits ('gams')
 check <- load("analysis/output/OxygenDebt/gam_predictions.RData")
 if (check != "pars") {
-  stop("Error loading gam predictions!\n\tTry rerunning model_3_indicators.R")
+  stop("Error loading gam predictions!\n\tTry rerunning model_3_spatial_predictions.R")
 }
 rm(check)
 
-
+# create table of indicators by year
 tab1 <-
   sapply(pars, function(parsy) {
     with(parsy, tapply(oxygendebt, Basin, mean, na.rm = TRUE))
   })
 
 # write out
-write.csv(tab1, file = "analysis/output/OxygenDebt/indicator_table.csv", row.names = FALSE)
+write.csv(tab1, file = "analysis/output/OxygenDebt/indicator_table_by_year.csv", row.names = FALSE)
 
 # done -------------------
 
