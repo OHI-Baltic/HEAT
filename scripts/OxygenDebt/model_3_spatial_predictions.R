1a70d766a09bc9d18955b883944b3bfa4b616f01
colin millar <colinpmillar@gmail.com>
2017-01-09 13:54:52 +0100

    spatial modelling revision

--- scripts/OxygenDebt/model_3_spatial_predictions.R
+++ scripts/OxygenDebt/model_3_spatial_predictions.R
@@ -44,40 +44,36 @@ if (FALSE) {
 # *     O2_deficit at lower halocline
 # *     O2_slope slope
 
-#names(gams)
-#g <- gams[[3]]
-predict_surfaces <- function(g) {
+# create prediction data
+surfaces <- dplyr::rename(data.frame(bathy), x = coords.x1, y = coords.x2)
+surfaces$yday <- 1 # predict at january 1st - this will be dropped later anyway
 
-  # predict
-  # create prediction data
-  pred <- dplyr::rename(data.frame(bathy), x = coords.x1, y = coords.x2)
-  pred$yday <- 1
-
-  pred_one_year <- function(y) {
-    # create design matrix for spatial  and annual variation only
-    Xs <- predict(g, newdata = dplyr::mutate(pred, Year = y), type = "lpmatrix")
-    Xs[,grep("yday",colnames(Xs))] <- 0
-
-    # make predictions
-    c(Xs %*% coef(g))
-  }
-
-  out <- sapply(2010:2015, pred_one_year)
-  colnames(out) <- 2010:2015
-
-  # return
-  out
+if (FALSE) {
+  # reduce size of prediction grid for testing
+  surfaces <- surfaces[sample(1:nrow(surfaces), 1000),]
 }
 
-# fit surfaces for each
-what <- c(#"sali_surf", "sali_dif",
-          "halocline", "depth_gradient",
-          "O2def_below_halocline", "O2def_slope_below_halocline")
-
+# create prediction data for each year
 surfaces <-
-  sapply(what,
-    function(what) predict_surfaces(gams[[what]]),
-    simplify = FALSE)
+  do.call(rbind,
+    lapply(2011:2015, function(y) cbind(surfaces, Year = y))
+  )
+
+# do predictions
+surfaces <-
+  do.call(cbind.data.frame,
+    c(list(surfaces),
+      sapply(gams,
+        function(g)
+        {
+          # set seasonal to zero for annual variation only
+          g$coefficients[grep("yday",names(coef(g)))] <- 0
+          # make predictions
+          exp(predict(g, newdata = surfaces))
+        },
+        simplify = FALSE)
+      )
+  )
 
 # calculate derived quantities
 surfaces$depth_change_point1 <- surfaces$halocline - 1.0 * surfaces$depth_gradient
@@ -85,7 +81,7 @@ surfaces$depth_change_point2 <- surfaces$halocline + 1.0 * surfaces$depth_gradie
 
 # check
 if (FALSE) {
-  lapply(surfaces, summary)
+  summary(surfaces)
 }
 
 # From these surfaces, compute volume specific oxygen deficit.
@@ -96,57 +92,42 @@ if (FALSE) {
 # *         this results in a surface of volume specific oxygen debt.
 
 # * Other useful quantities are - oxygen deficit at maximum depth.
-pars <-
-  sapply(paste(2010:2015),
-    function(y) {
-      out <-
-        data.frame(halocline = surfaces$halocline[,y],
-                   depth_change_point1 = surfaces$depth_change_point1[,y],
-                   depth_change_point2 = surfaces$depth_change_point2[,y],
-                   O2def_below_halocline       = surfaces$O2def_below_halocline[,y],
-                   O2def_slope_below_halocline = surfaces$O2def_slope_below_halocline[,y],
-                   depth = bathy$depth,
-                   Basin = bathy$Basin,
-                   x = sp::coordinates(bathy)[,1],
-                   y = sp::coordinates(bathy)[,2])
 
-      # only make predctions where depth extends below halocline
-      out <- out[out$halocline < out$depth,]
+surfaces$oxygendebt <-
+  sapply(1:nrow(surfaces),
+    function(i) {
+      if (surfaces$halocline[i] >= surfaces$depth[i]) return (NA_real_)
+      depths <- seq(surfaces$halocline[i], surfaces$depth[i], by = 1)
+      mean(oxy_profile(depths, surfaces[i,])) # sum(o2) / volume
+    })
 
-      out$oxygendebt <-
-        sapply(1:nrow(out),
-               function(i) {
-                 depths <- seq(out$halocline[i], out$depth[i], by = 1)
-                 mean(oxy_profile(depths, out[i,])) # sum(o2) / volume
-               })
-      out
-    },
-    simplify = FALSE)
 
 # checks
 if (FALSE) {
-  lapply(pars, summary)
+  summary(surfaces)
 
   # plot surfaces to check
-  tmp <- pars[["2010"]]
+  tmp <- surfaces[surfaces$Year == 2011,]
   sp::coordinates(tmp) <- c("x", "y")
   sp::proj4string(tmp) <- sp::CRS("+proj=utm +zone=34 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0")
 
-  sp::plot(tmp, col = rev(viridis::magma(50, alpha=0.5))[cut(tmp$halocline, 50)], pch = ".")
+  sp::plot(tmp, col = rev(viridis::magma(50, alpha=0.5))[cut(tmp$halocline, 50)], pch = 16, cex = 0.5)
   sp::plot(helcom, add = TRUE, border = "red")
+  title(main = "Halocline depth")
 
-  sp::plot(tmp, col = rev(viridis::magma(50, alpha=0.5))[cut(tmp$O2def_below_halocline, 50)], pch = ".")
+  sp::plot(tmp, col = rev(viridis::magma(50, alpha=0.5))[cut(tmp$O2def_below_halocline, 50)], pch = 16, cex = 0.5)
   sp::plot(helcom, add = TRUE, border = "red")
+  title(main = "O2def below halocline")
 
-  sp::plot(tmp, col = rev(viridis::magma(50, alpha=0.5))[cut(tmp$oxygendebt, 50)], pch = ".")
+  sp::plot(tmp, col = rev(viridis::magma(50, alpha=0.5))[cut(tmp$oxygendebt, 50)], pch = 16, cex = 0.5)
   sp::plot(helcom, add = TRUE, border = "red")
+  title(main = "Volume specific oxygen debt")
 
-  sapply(pars, function(x) summary(x$oxygendebt))
 }
 
 # write out --------------
 
-save(pars, file = "analysis/output/OxygenDebt/gam_predictions.RData")
+save(surfaces, file = "analysis/output/OxygenDebt/gam_predictions.RData")
 
 
 # done -------------------
