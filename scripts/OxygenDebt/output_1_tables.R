b655f303d8cda32e308266a0b83f6a4604fff75c
colin millar <colinpmillar@gmail.com>
2017-01-09 19:58:13 +0100

    almost finished tables

--- scripts/OxygenDebt/output_1_tables.R
+++ scripts/OxygenDebt/output_1_tables.R
@@ -15,38 +15,32 @@ t0 <- proc.time()
 
 # load gam predictions ('pars')
 check <- load("analysis/output/OxygenDebt/gam_predictions.RData")
-if (check != "pars") {
+if (check != "surfaces") {
   stop("Error loading gam predictions!\n\tTry rerunning model_3_spatial_predictions.R")
 }
 rm(check)
 
 # create table of indicators by year
-ES_tab <-
-  sapply(pars, function(parsy) {
-    with(parsy, tapply(oxygendebt, Basin, mean, na.rm = TRUE))
-  })
-ES_tab <- cbind.data.frame(Basin = row.names(ES_tab), ES_tab)
-rownames(ES_tab) <- NULL
-
-# expand into data table form
-ES_tab <- tidyr::gather(ES_tab, "Year", "ES", -1)
+#ES <- with(surfaces, tapply(oxygendebt, list(Basin, Year), mean, na.rm = TRUE))
+ES <- with(surfaces, tapply(oxygendebt, list(Basin), mean, na.rm = TRUE))
+out <- do.call(expand.grid, c(dimnames(ES), KEEP.OUT.ATTRS = FALSE))
+out$ES <- c(ES)
 
 # add in auxilliary information
 # read profile fits
 profiles <- read.csv("analysis/output/OxygenDebt/profiles.csv")
-N_tab <- with(profiles, tapply(O2def_slope_below_halocline, list(Basin, Year), function(x) sum(!is.na(x))))
-N_tab <- cbind.data.frame(Basin = row.names(N_tab), N_tab)
-rownames(N_tab) <- NULL
+#N <- with(profiles, tapply(O2def_slope_below_halocline, list(Basin, Year), function(x) sum(!is.na(x))))
+N <- with(profiles, tapply(O2def_slope_below_halocline, list(Basin), function(x) sum(!is.na(x))))
+out$N <- c(N)
 
-# expand into data table form
-N_tab <- tidyr::gather(N_tab, "Year", "N", -1)
-N_tab[is.na(N_tab)] <- 0
+# inspect
+if (FALSE) {
+  out
+}
 
-# join
-out <- dplyr::left_join(ES_tab, N_tab, by = c("Basin", "Year"))
 
 # write out
-write.csv(out, file = "analysis/output/OxygenDebt/indicator_table_by_year.csv", row.names = FALSE)
+write.csv(out, file = "analysis/output/OxygenDebt/indicator_table.csv", row.names = FALSE)
 
 # done -------------------
 
