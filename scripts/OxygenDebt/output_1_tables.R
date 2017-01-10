c6742ce0385d49ade74536e3aa58df659ac5eff4
colin millar <colinpmillar@gmail.com>
2017-01-10 16:29:23 +0100

    provide output by year

--- scripts/OxygenDebt/output_1_tables.R
+++ scripts/OxygenDebt/output_1_tables.R
@@ -20,9 +20,15 @@ if (check != "surfaces") {
 }
 rm(check)
 
-# create table of indicators by year
-#ES <- with(surfaces, tapply(oxygendebt, list(Basin, Year), mean, na.rm = TRUE))
-#names(dimnames(ES)) <- c("Basin", "Year")
+
+# create table of indicators by year and basin
+ES_y <- with(surfaces, tapply(oxygendebt, list(Basin, Year), mean, na.rm = TRUE))
+names(dimnames(ES_y)) <- c("Basin", "Year")
+out_y <- do.call(expand.grid, c(dimnames(ES_y), KEEP.OUT.ATTRS = FALSE))
+out_y$ES <- c(ES_y)
+
+
+# create table of indicators by basin
 ES <- with(surfaces, tapply(oxygendebt, list(Basin), mean, na.rm = TRUE))
 names(dimnames(ES)) <- "Basin"
 out <- do.call(expand.grid, c(dimnames(ES), KEEP.OUT.ATTRS = FALSE))
@@ -36,18 +42,25 @@ out$ES_SD <- c(ES)
 # add in auxilliary information
 # read profile fits
 profiles <- read.csv("analysis/output/OxygenDebt/profiles.csv")
-#N <- with(profiles, tapply(O2def_slope_below_halocline, list(Basin, Year), function(x) sum(!is.na(x))))
+N_y <- with(profiles, tapply(O2def_slope_below_halocline, list(Basin, Year), function(x) sum(!is.na(x))))
+out_y$N <- c(N_y)
+
 N <- with(profiles, tapply(O2def_slope_below_halocline, list(Basin), function(x) sum(!is.na(x))))
 out$N <- c(N)
 
 # inspect
 if (FALSE) {
   out
+  out_y
+
+  # check
+  cbind(with(out_y, tapply(N, Basin, sum)), out$N)
 }
 
 
 # write out
 write.csv(out, file = "analysis/output/OxygenDebt/indicator_table.csv", row.names = FALSE)
+write.csv(out_y, file = "analysis/output/OxygenDebt/indicator_table_by_year.csv", row.names = FALSE)
 
 # done -------------------
 
