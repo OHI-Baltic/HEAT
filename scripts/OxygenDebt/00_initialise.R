ea1cd321acde4321910a1bcd2ae4ffcbcb951a3d
colin millar <colinpmillar@gmail.com>
2017-08-03 12:06:06 +0200

    set strings as factors to FALSE for analysis

--- scripts/OxygenDebt/00_initialise.R
+++ scripts/OxygenDebt/00_initialise.R
@@ -1,3 +1,7 @@
 # load header function into top of search list
 while("tools:oxydebt_funs" %in% search()) detach("tools:oxydebt_funs")
 sys.source("scripts/OxygenDebt/00_header.R", envir = attach(NULL, name = "tools:oxydebt_funs"))
+
+# set some options
+options(stringsAsFactors = FALSE)
+
