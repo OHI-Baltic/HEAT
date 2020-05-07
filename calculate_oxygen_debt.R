7b99964190e075c0b4dde029145b5f2e51385371
eleanorecc <eleanorecampbell@ucsb.edu>
2020-05-07 18:40:51 +0200

    calculating for all year, master script

--- calculate_oxygen_debt.R
+++ calculate_oxygen_debt.R
@@ -1,12 +1,13 @@
 ## Running the HEAT assessment tool to Calculate Oxygen Debt for BHI
 
+## set main/root directory based on working location
+dirmain <- here::here()
+bhiremote <- "/Users/eleanorecampbell/Desktop/GitHub/bhi-data/BHI 2.0/Goals/CW/EUT"
+# bhiremote <- "/mnt/data/ellie/bhi_share/BHI 2.0/Goals/CW/EUT/HEATData/"
+
 ## load header function into top of search list
 source("scripts/OxygenDebt/00_initialise.R")
 
-
-## no config.json file available, so manually specify these as a list here
-config <- list()
-config[["years"]] <- 2014:2018
 
 ## refer to readme in main directory for more details on how the procedure was adjuested for BHI calculations
 ## e.g. original ftp server was not available for downloading data, so some changes were made:
@@ -16,6 +17,7 @@ config[["years"]] <- 2014:2018
 ## auxilary file with surface depths, salmod_max_depth, and max_depth was... (recreate??)
 
 
+## NOTES ----
 ## notes on functions: 
 
 ## O2satFun in scripts/OxygenDebt/utils/data_functions.R:
@@ -29,11 +31,20 @@ config[["years"]] <- 2014:2018
 ## oxygen deficit model based on oxygen slope computed from the data... (???)
 
 
-## get scripts to run
+## RUN ALL SCRIPTS FOR SPECIFIED YEARS ----
+
+## no config.json file available, so manually specify these as a list here
+config <- list()
+config[["years"]] <- 2000:2019
+
+## get scripts to run, and all scripts in correct order
 files <- paste0(
   "scripts/OxygenDebt/",
   dir("scripts/OxygenDebt/", pattern = "^(data|input|model|output)_.*[.]R$")
 )
+for (file in sort(files)) {
+  source(file, echo = TRUE, keep.source = TRUE)
+}
 
 ## order of scripts execution:
 # source("scripts/OxygenDebt/data_2_make_assessment_area.R")
@@ -46,19 +57,14 @@ files <- paste0(
 # source("scripts/OxygenDebt/output_1_tables.R")
 # source("scripts/OxygenDebt/output_2_tables_corrected.R")
 
-## run all scripts in correct order
-for (file in sort(files)) {
-  source(file, echo = TRUE, keep.source = TRUE)
-}
 
+## VISUALIZE FITTED PROFILES ----
 
 ## oxygen input data and example plot of fitted haloclines... (?) 
-oxy <- read.csv("analysis/input/OxygenDebt/oxy.csv")
-str(oxy)
-ID <- 8492
-data <- oxy[oxy$ID == ID,]
-fit <- doonefit_full(data, ID = ID)
+# oxy <- read.csv("analysis/input/OxygenDebt/oxy.csv")
+# str(oxy)
+# ID <- 8492
+# data <- oxy[oxy$ID == ID,]
+# fit <- doonefit_full(data, ID = ID)
 # plot(pred$Depth, pred$fit, type = "l", col = "red")
 # points(pdata$Depth, pdata$Oxygen_deficit, col = "red")
-
-
