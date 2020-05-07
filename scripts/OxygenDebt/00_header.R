b3a30e30fe47f4d2d3fd12ff81187d15f14b24b3
eleanorecc <eleanorecampbell@ucsb.edu>
2020-05-07 14:34:41 +0200

    want to keep config in envir throughout scripts calculations

--- scripts/OxygenDebt/00_header.R
+++ scripts/OxygenDebt/00_header.R
@@ -4,41 +4,40 @@
 #
 # ----------------------------
 
-header <-
-function(phase = c("data", "input", "model", "output"))
-{
+header <- function(phase = c("data", "input", "model", "output")){
+  
   phase <- match.arg(phase)
   # phase specific packages -------------------
-  pkg <-
-    switch(phase,
-      data = c("sp", "rgdal", "rgeos", "dplyr"),
-      input = c("lubridate"),
-      model = c("stats", "survival", "dplyr", "mgcv", "sp", "rgdal"),
-      output = c("tidyr", "dplyr")
-    )
-
+  pkg <- switch(
+    phase,
+    data = c("sp", "rgdal", "rgeos", "dplyr"),
+    input = c("lubridate"),
+    model = c("stats", "survival", "dplyr", "mgcv", "sp", "rgdal"),
+    output = c("tidyr", "dplyr")
+  )
+  
   # common packages -------------------
   pkg <- unique(c(pkg, "viridis", "gplots", "jsonlite"))
-
+  
   # install dependencies used in the analysis
   newpkg <- setdiff(pkg, installed.packages())
   if (length(newpkg)) install.packages(newpkg, dependencies = TRUE, quiet = TRUE)
-
+  
   # always load certain packages -------------------
   always_load <- c("mgcv")
   if (always_load %in% pkg) {
     tmp <- sapply(intersect(always_load, pkg), library, character.only = TRUE, quietly = TRUE, pos = 3)
   }
-
+  
   # load utils into oxydebt_funs namespace
-  tmp <-
-    sapply(dir("scripts/OxygenDebt/utils"),
-           function(x) {
-           sys.source(paste0("scripts/OxygenDebt/utils/", x), envir = environment(header))
-           })
-
+  tmp <- sapply(
+    dir("scripts/OxygenDebt/utils"),
+    function(x) {
+      sys.source(paste0("scripts/OxygenDebt/utils/", x), envir = environment(header))
+    })
+  
   # clear workspace -------------------
-  rm(list = ls(envir = .GlobalEnv), envir = .GlobalEnv)
+  rm(list = setdiff(ls(envir = .GlobalEnv), "config"), envir = .GlobalEnv)
 }
 
 
